2cfcb12cc37c183b6169ce086772dd376ebeeabb
Navi026 <karlos123jun@gmail.com>
2022-08-01 09:09:20 -0500

    feat: added user model

---
+++ server/models/user-model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true,'the name is required']
+    },
+    firstlastname: {
+        type: String,
+        required: [true,'the first last name is required']
+    },
+    secondlastname: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true,'the email is required']
+    },
+    pass: {
+        type: String,
+        required: [true,'the pass is required']
+    },
+    dateofbirth: {
+        type: Date,
+        required: [true,'the date of birth is required']
+    }
+});
+
+module.exports = mongoose.model("user", userSchema);
