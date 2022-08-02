9766dbac6fd795c1c95e89e67a6b2ec1b9e85e40
AngelaMTC <tc.angela.m@gmail.com>
2022-08-02 18:49:49 -0500

    feat: A new feature has been added, the post-route file has been created in the routes folder (routes/post-routes) to perform CRUD, the request is POST.

--- server/routes/post-route.js
+++ server/routes/post-route.js
@@ -20,6 +20,18 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-
+router.post("/", async (req, res) => {
+  try {
+    const post = new Post({
+      title: req.body.title,
+      description: req.body.description,
+      completed: req.body.completed,
+    });
+    const postSaved = await post.save();
+    res.json(postSaved);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
