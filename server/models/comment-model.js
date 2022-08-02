bb40261144587ba215af5a838890250baa2ea3a5
Susana Alba Najera <1fsusanaalbanajera@gmail.com>
2022-08-01 20:57:47 -0500

    Feat: Comment model was created

---
+++ server/models/comment-model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+    Idpost: {
+        type: Schema.Types.ObjectId,
+        ref: "post",
+        required: [true, "The IdPost is required"],
+    },
+    iduser: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+        required: [true, "The IdUser is required"],
+    },
+    comment: {
+        type: Text
+    }
+});
+
+module.exports = mongoose.model("comment", commentSchema);
